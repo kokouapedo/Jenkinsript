74b75d85f91c1a80d23eb02287e2ec48dad1b4ed
root <root@ubuntu20.04>
2023-02-06 12:32:12 +0700

    pkg file

---
+++ ubpkg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Author : kokou
+
+#--------------------------------Script needed before Jenken installation--------#
+
+#--------------------------------Update Ubuntu packages and all installed applications------#
+
+
+sudo apt-get update -y
+
+
+sudo apt-get upgrade -y
+
+
+#---------------------------------JDK(Java)-installation----------------------------------#
+
+
+sudo apt install openjdk-11-jdk -y
+
+#-------------------------------Java version-----------------------------------#
+
+
+java -version
+
+
+#-------------------------------Update our package list again-------------------#
+
+
+sudo apt-get update -y
+
+
+
